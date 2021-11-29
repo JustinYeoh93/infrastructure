70174ba902f97cba67b6016b39c27f555b8213ae
Justin <jerwei.yeoh@exwzd.com>
2021-11-29 10:02:33 +0900

    Removed monitoring

--- tfcloud/main.tf
+++ tfcloud/main.tf
@@ -8,14 +8,3 @@ resource "tfe_workspace" "minecraft" {
   file_triggers_enabled = false
   working_directory     = "aws/minecraft"
 }
-
-resource "tfe_workspace" "monitoring" {
-  name                  = "monitoring"
-  organization          = data.tfe_organization.justinyeoh.name
-  tag_names             = ["tfmanaged"]
-  allow_destroy_plan    = false
-  auto_apply            = true
-  execution_mode        = "remote"
-  file_triggers_enabled = false
-  working_directory     = "aws/monitoring"
-}
