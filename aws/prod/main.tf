68920b6b43198975f24ac25eec5f8c3343a37158
Justin <jerwei.yeoh@exwzd.com>
2021-11-30 20:40:30 +0900

    Fix secret typo

--- aws/prod/main.tf
+++ aws/prod/main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "b" {
   bucket_prefix = "my-prod-tf-test-bucket"
-  acl    = "private"
+  acl           = "private"
 
   tags = {
     Name        = var.aws_secretsmanager_secret_version.s3_prod_secret
--- aws/prod/secrets.tf
+++ aws/prod/secrets.tf
@@ -6,6 +6,6 @@ data "aws_secretsmanager_secret_version" "s3_prod_secret" {
   secret_id = data.aws_secretsmanager_secret.justin_secret.id
 }
 
-output name {
-  value       = data.aws_secretsmanager_secret_version.secret_string
+output "name" {
+  value = data.aws_secretsmanager_secret_version.justin_secret.secret_string
 }
