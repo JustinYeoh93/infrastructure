06b1f6d5441a18f459e5126444dcdc06fea67d52
Justin <jerwei.yeoh@exwzd.com>
2021-11-30 19:47:06 +0900

    Fix assumerole typo

--- aws/prod/provider.tf
+++ aws/prod/provider.tf
@@ -1,3 +1,5 @@
 provider "aws" {
-  role_arn = "arn:aws:iam::021498413756:role/terraform-admin-role"
+    assume_role {
+        role_arn = "arn:aws:iam::021498413756:role/terraform-admin-role"
+    }
 }
