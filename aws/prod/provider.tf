25b8da78f38740c7d71215b00b675c238eaf3bba
Justin <jerwei.yeoh@exwzd.com>
2021-12-05 20:41:16 +0900

    Update

--- aws/prod/provider.tf
+++ aws/prod/provider.tf
@@ -1,4 +1,5 @@
 provider "aws" {
+  region = "ap-northeast-1"
   assume_role {
     role_arn = "arn:aws:iam::021498413756:role/terraform-admin-role"
   }
--- aws/prod/secrets.tf
+++ aws/prod/secrets.tf
@@ -6,9 +6,16 @@ data "aws_secretsmanager_secret_version" "s3_prod_secret" {
   secret_id = data.aws_secretsmanager_secret.justin_secret.id
 }
 
+data "aws_ssm_parameter" "foo" {
+  name = "/foo/bar"
+}
+
+output "ssm_parameter" {
+  value = data.aws_ssm_parameter.foo.value
+}
+
 output "name" {
   value     = data.aws_secretsmanager_secret_version.s3_prod_secret.secret_string
-  sensitive = true
 }
 
 output "id" {
