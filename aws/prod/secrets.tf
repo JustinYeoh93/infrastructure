3236aa7d5e0924e4f32b9f4e959f6e69c6dfd278
Justin <jerwei.yeoh@exwzd.com>
2021-12-05 21:01:03 +0900

    Removed justin/test secret to reduce cost

--- aws/prod/secrets.tf
+++ aws/prod/secrets.tf
@@ -1,11 +1,3 @@
-data "aws_secretsmanager_secret" "justin_secret" {
-  arn = "arn:aws:secretsmanager:ap-northeast-1:021498413756:secret:justin/secret-XyxJVU"
-}
-
-data "aws_secretsmanager_secret_version" "s3_prod_secret" {
-  secret_id = data.aws_secretsmanager_secret.justin_secret.id
-}
-
 data "aws_ssm_parameter" "foo" {
   name = "/foo/bar"
 }
