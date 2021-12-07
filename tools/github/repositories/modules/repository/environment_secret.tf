4f5074fecf8407306737fec82a590031e56651a4
Justin <jerwei.yeoh@exwzd.com>
2021-12-07 10:02:18 +0900

    Added test

--- tools/github/repositories/modules/repository/environment_secret.tf
+++ tools/github/repositories/modules/repository/environment_secret.tf
@@ -1,7 +1,7 @@
 resource "github_actions_environment_secret" "environment_secrets" {
   for_each        = { for v in local.environment_secrets : format("%s__%s", v.environment_name, v.secret_name) => v }
   repository      = github_repository.repository.name
-  environment     = each.value.environment_name
+  environment     = github_repository_environment.environments[each.value.environment_name].environment
   secret_name     = each.value.secret_name
   plaintext_value = each.value.encrypted ? null : each.value.secret_value
   encrypted_value = each.value.encrypted ? each.value.secret_value : null
--- tools/github/repositories/test.tf
+++ tools/github/repositories/test.tf
@@ -1,4 +1,4 @@
 module "test" {
-    module = "./modules/repository"
-    name = "test"
+  module = "./modules/repository"
+  name   = "test"
 }
